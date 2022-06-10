ee8836935810e3126875590e14db5b62c54700f8
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-06-10 17:17:50 -0300

    continuando aula

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,38 @@ const data = [
   { name: "Érica", age: 16, sallary: 0, driverLicense: false },
 ];
 
-// 1 -
+// 1 - Reverse
+
+const reverseData = data.reverse();
+
+console.log(reverseData);
+
+// 2 - find
+
+const highestSallary = data.find((user) => user.sallary > 5000);
+
+console.log(highestSallary);
+
+// 3 - findIndex
+
+const lowestSallary = data.findIndex(
+  (user) => user.sallary > 0 && user.sallary < 2000
+);
+
+console.log(lowestSallary);
+
+data[lowestSallary].sallary += 200;
+
+console.log(data);
+
+// 4 - includes
+const numbers = [1, 2, 3, 4, 5];
+
+const hasFour = numbers.includes(4);
+
+console.log(hasFour);
+
+// 5 - map
+data.map((user) => (user.newsletter = false));
+
+console.log(data);

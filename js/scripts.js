3646d46246b0240a40c655c1d75b2c413ca3fb55
Matheus Battisti <matheusbudkewicz@gmail.com>
2022-06-10 16:40:09 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const data = [
+  { name: "Matheus", age: 31, sallary: 2000, driverLicense: true },
+  { name: "João", age: 18, sallary: 1500, driverLicense: false },
+  { name: "Mariana", age: 22, sallary: 4000, driverLicense: true },
+  { name: "Pedro", age: 50, sallary: 7200, driverLicense: true },
+  { name: "Érica", age: 16, sallary: 0, driverLicense: false },
+];
+
+// 1 -
